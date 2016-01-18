3d1391c2efcd4f28b77bcae44ed76dd5ae2a8047
Lucas Marcelo Suárez <suarezlucas28@gmail.com>
2016-01-18 18:08:02 -0300

    a

--- server.js
+++ server.js
@@ -305,12 +305,107 @@ var cstr = { Host : 'localhost:2638', Server : 'integrado',
 												}
 												string+="ORDER BY 1, 2"
 		}	
-		
+				
 		conn.exec(string, function(err, row){
 			res.json({ data : row });
 		});
 	});
 	
+	//list balance comprobado
+	restapi.get('/api/v1/balancecomprobado/list/:empresa/:periodo/:cuentad/:cuentah/:mesanterior/:mesd/:nivel/:moneda', function(req, res){
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		var empresa = req.params.empresa;
+		var periodo = req.params.periodo;
+		var cuentad = req.params.cuentad;
+		var cuentah = req.params.cuentah;
+		var mesd = req.params.mesd;
+		var mesh = req.params.mesanterior;
+		var nivel = req.params.nivel;
+		var moneda = req.params.moneda;
+		
+		var string = "SELECT " +
+						"DBA.PLANCTA.Cod_Empresa,"+
+						"DBA.PLANCTA.CodPlanCta,"+
+						"DBA.PLANCTA.Nombre,"+
+						"DBA.PLANCTA.Nivel,"+
+						"DBA.PLANCTA.Imputable,"+
+						"ifnull((SELECT "
+									if (moneda == 'local'){
+										string+="(CASE DBA.PLANCTA.TipoSaldo "+
+													"WHEN 'D' then cast(SUM (A2.TotalDB) -  SUM(A2.TotalCR) as decimal(20,0)) "+
+													"ELSE cast(SUM (A2.TotalCR) -  SUM(A2.TotalDB) as decimal(20,0)) END)"
+									} else {
+										string+="(CASE DBA.PLANCTA.TipoSaldo "+
+													"WHEN 'D' then cast(SUM (A2.TotalDBME) -  SUM(A2.TotalCRME) as decimal(20,0)) "+
+													"ELSE cast(SUM (A2.TotalCRME) -  SUM(A2.TotalDBME) as decimal(20,0)) END)"
+									}
+							string+="FROM "+ 
+									"DBA.ACUMPLAN A2 "+
+								"WHERE "+
+									"A2.Cod_Empresa = DBA.PLANCTA.Cod_Empresa "+
+									"AND A2.Periodo = DBA.PLANCTA.Periodo "+
+									"AND A2.CodPlanCta  = DBA.PLANCTA.CodPlanCta "+
+									"AND (DBA.PLANCTA.CodPlanCta >= '"+cuentad+"') "+  
+									"AND (DBA.PLANCTA.CodPlanCta <= '"+cuentah+"') "+
+									"AND A2.AnhoMes    >= "+parseInt(periodo.toString()+mesd.toString())+" "+
+									"AND A2.AnhoMes    =  "+parseInt(periodo.toString()+mesd.toString())+" "
+									if (nivel > 0)
+										string+="AND (DBA.PLANCTA.Nivel <= "+nivel+") "
+							string+="),0,(SELECT "
+											if (moneda == 'local'){
+												string+="(CASE DBA.PLANCTA.TipoSaldo "+
+															"WHEN 'D' then cast(SUM (A2.TotalDB) -  SUM(A2.TotalCR) as decimal(20,0)) "+
+															"ELSE cast(SUM (A2.TotalCR) -  SUM(A2.TotalDB) as decimal(20,0)) END)"
+											} else {
+												string+="(CASE DBA.PLANCTA.TipoSaldo "+
+															"WHEN 'D' then cast(SUM (A2.TotalDBME) -  SUM(A2.TotalCRME) as decimal(20,0)) "+
+															"ELSE cast(SUM (A2.TotalCRME) -  SUM(A2.TotalDBME) as decimal(20,0)) END)"
+											}
+							 string+="FROM "+
+											"DBA.ACUMPLAN A2 "+
+									 "WHERE "+
+											"A2.Cod_Empresa = DBA.PLANCTA.Cod_Empresa "+
+											"AND A2.Periodo     = DBA.PLANCTA.Periodo "+
+											"AND A2.CodPlanCta  = DBA.PLANCTA.CodPlanCta "+
+											"AND (DBA.PLANCTA.CodPlanCta >= '"+cuentad+"') "+  
+											"AND (DBA.PLANCTA.CodPlanCta <= '"+cuentah+"') "+
+											"AND A2.AnhoMes    >= "+parseInt(periodo.toString()+mesd.toString())+" "+
+											"AND A2.AnhoMes    =  "+parseInt(periodo.toString()+mesd.toString())+" "
+											if (nivel > 0)
+												string+="AND (DBA.PLANCTA.Nivel <= "+nivel+") "
+							string+=")) as SALDO_ANTERIOR,"
+						if (moneda == 'local'){
+							string+="ifnull(cast(DBA.ACUMPLAN.TotalDb as decimal(20,0)),0,cast(DBA.ACUMPLAN.TotalDb as decimal(20,0))) as TOTAL_DEBITO,"+
+									"ifnull(cast(DBA.ACUMPLAN.TotalCr as decimal(20,0)),0,cast(DBA.ACUMPLAN.TotalCr as decimal(20,0))) as TOTAL_CREDITO,"
+						} else {
+							string+="ifnull(cast(DBA.ACUMPLAN.TotalDbME as decimal(20,0)),0,cast(DBA.ACUMPLAN.TotalCr as decimal(20,0))) as TOTAL_DEBITO,"+
+									"ifnull(cast(DBA.ACUMPLAN.TotalCrME as decimal(20,0)),0,cast(DBA.ACUMPLAN.TotalCrME as decimal(20,0))) as TOTAL_CREDITO,"
+						}
+				string+="DBA.PLANCTA.TipoSaldo,"+
+						"(CASE DBA.PLANCTA.TipoSaldo "+
+							"WHEN 'D' THEN (SALDO_ANTERIOR + TOTAL_DEBITO)-TOTAL_CREDITO "+
+							"ELSE (SALDO_ANTERIOR + TOTAL_CREDITO)-TOTAL_DEBITO END) AS SALDO "+
+					"FROM DBA.PLANCTA  LEFT OUTER JOIN DBA.ACUMPLAN  ON "+
+						"DBA.PLANCTA.Cod_Empresa = DBA.ACUMPLAN.Cod_Empresa "+
+						"AND DBA.PLANCTA.Periodo = DBA.ACUMPLAN.Periodo "+
+						"AND DBA.PLANCTA.CodPlanCta = DBA.ACUMPLAN.CodPlanCta "+
+						"AND (DBA.PLANCTA.CodPlanCta >= '"+cuentad+"') "+  
+						"AND (DBA.PLANCTA.CodPlanCta <= '"+cuentah+"') "+
+						"AND DBA.ACUMPLAN.AnhoMes >= "+parseInt(mesh.toString())+" "+
+						"AND DBA.ACUMPLAN.AnhoMes = "+parseInt(mesh.toString())+" "
+						if (nivel > 0)
+							string+="AND (DBA.PLANCTA.Nivel <= "+nivel+") "
+				string+="WHERE "+
+						"DBA.PLANCTA.cod_empresa = '"+empresa+"' "+
+						"AND DBA.PLANCTA.periodo = "+periodo+" "
+						 if (nivel > 0)
+							string+="AND (DBA.PLANCTA.Nivel <= "+nivel+") "
+				string+="ORDER BY DBA.PLANCTA.CodPlanCta"
+                console.log(string)                                                       
+		conn.exec(string, function(err, row){
+			res.json({ data : row });
+		});
+	});	
 
 	restapi.listen(3000);
 	restapi.use(function(req, res, next){
@@ -325,47 +420,6 @@ var cstr = { Host : 'localhost:2638', Server : 'integrado',
 });
 
 /*
- SELECT 
-    DBA.PLANCTA.Cod_Empresa,
-    DBA.PLANCTA.CodPlanCta,
-    DBA.PLANCTA.Nombre,
-    DBA.PLANCTA.Nivel,
-	DBA.PLANCTA.Imputable,		
-    (SELECT 
-        cast(SUM (A2.TotalDB) -  SUM(A2.TotalCR) as decimal(20,0))
-    FROM 
-        DBA.ACUMPLAN A2
-    WHERE 
-        A2.Cod_Empresa = DBA.PLANCTA.Cod_Empresa
-        AND A2.Periodo     = DBA.PLANCTA.Periodo
-        AND A2.CodPlanCta  = DBA.PLANCTA.CodPlanCta
-        AND A2.AnhoMes    >= 201111
-        AND A2.AnhoMes    =  201111
-    ) as SALDO_ANTERIOR,
-    (SELECT 
-        cast(SUM (A2.TotalDBME) -  SUM(A2.TotalCRME) as decimal(20,0))
-    FROM 
-        DBA.ACUMPLAN A2
-    WHERE 
-        A2.Cod_Empresa = DBA.PLANCTA.Cod_Empresa
-        AND A2.Periodo = DBA.PLANCTA.Periodo
-        AND A2.CodPlanCta  = DBA.PLANCTA.CodPlanCta
-        AND A2.AnhoMes    >= 201111
-        AND A2.AnhoMes    =  201111
-    ) as SALDO_ANTERIORME,
-    cast(DBA.ACUMPLAN.TotalDb as decimal(20,0)) as TOTAL_DEBITO,
-    cast(DBA.ACUMPLAN.TotalCr as decimal(20,0)) as TOTAL_CREDITO,
-    cast(DBA.ACUMPLAN.TotalDbME as decimal(20,0)) as TOTAL_DEBITOME,
-    cast(DBA.ACUMPLAN.TotalCrME as decimal(20,0)) as TOTAL_CREDITOME,
-  	DBA.PLANCTA.TipoSaldo	
-FROM DBA.PLANCTA  LEFT OUTER JOIN DBA.ACUMPLAN  ON
-      DBA.PLANCTA.Cod_Empresa = DBA.ACUMPLAN.Cod_Empresa  AND
-      DBA.PLANCTA.Periodo     = DBA.ACUMPLAN.Periodo      AND
-      DBA.PLANCTA.CodPlanCta  = DBA.ACUMPLAN.CodPlanCta   AND
-      DBA.ACUMPLAN.AnhoMes   >= 201112 AND
-      DBA.ACUMPLAN.AnhoMes    = 201112 
-WHERE 
-    DBA.PLANCTA.cod_empresa = 'BT'
-    AND DBA.PLANCTA.periodo = 2011
+
 */
 
