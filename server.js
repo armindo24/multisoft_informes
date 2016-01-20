bb9b7ee704360d8b07e52238276d5d6fb9d78f48
Lucas Marcelo Suárez <suarezlucas28@gmail.com>
2016-01-20 17:54:56 -0300

    a

--- server.js
+++ server.js
@@ -309,11 +309,11 @@ var cstr = { Host : 'localhost:2638', Server : 'integrado',
 	});	
 	
 	//list diario comprobado
-	restapi.get('/api/v1/diariocomprobado/list/:empresa/:periodo/:tipoasiento/:fechad/:fechah/:autorizado', function(req, res){
+	restapi.get('/api/v1/diariocomprobado/list/:empresa/:tipoasiento/:fechad/:fechah/:autorizado', function(req, res){
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		
 		var empresa = req.params.empresa;
-		var periodo = req.params.periodo;
+		//var periodo = req.params.periodo;
 		var tipoasiento = req.params.tipoasiento;
 		var fechad = req.params.fechad;
 		var fechah = req.params.fechah;
@@ -321,25 +321,35 @@ var cstr = { Host : 'localhost:2638', Server : 'integrado',
 		
 		var string =  "SELECT "+
 							"dba.asientosdet.Cod_Empresa as EMPRESA,dba.asientosdet.NroTransac,dba.asientoscab.TipoAsiento,dba.asientoscab.NroCompr,dba.asientosdet.NroOrden as Linea,dba.asientosdet.CodPlanCta,"+
-							"dba.asientosdet.CodPlanAux,dba.asientosdet.Concepto,dba.asientosdet.DbCr,dba.asientosdet.Importe,dba.asientosdet.ImporteME,dba.asientoscab.Fecha,dba.asientosdet.DEBITO,"+
-							"dba.asientosdet.CREDITO,dba.asientosdet.DEBITOME as DEBITO_ME,dba.asientosdet.CREDITOME as CREDITO_ME,DBA.PLANCTA.Nombre as NOMBRECUENTA,DBA.PLANAUXI.Nombre as NOMBRECUENTAAUX,"+
+							"dba.asientosdet.CodPlanAux,dba.asientosdet.Concepto,dba.asientosdet.DbCr,dba.asientosdet.Importe,dba.asientosdet.ImporteME,date(dba.asientoscab.Fecha) as Fecha,cast(dba.asientosdet.DEBITO as decimal(20,0)) as DEBITO,"+
+							"cast(dba.asientosdet.CREDITO as decimal(20,0)) as CREDITO,cast(dba.asientosdet.DEBITOME as decimal(20,2)) as DEBITO_ME,cast(dba.asientosdet.CREDITOME as decimal(20,2)) as CREDITO_ME,DBA.PLANCTA.Nombre as NOMBRECUENTA,DBA.PLANAUXI.Nombre as NOMBRECUENTAAUX,"+
 							"dba.tipoasiento.Descrip as TIPOASIENTO,DBA.asientoscab.autorizado,DBA.asientoscab.cargadopor,DBA.asientoscab.fechacarga,DBA.asientoscab.autorizadopor,DBA.asientoscab.fechaautoriz,"+
 							"DBA.asientoscab.nroasiento,upper(dba.asientosdet.Concepto) as BUSCAR_CONCEPTO	"+
 					  "FROM "+
-							"DBA.PLANCTA,dba.asientoscab,dba.tipoasiento,dba.asientosdet,DBA.PLANAUXI "+
+							"(dba.asientosdet LEFT OUTER JOIN DBA.PLANAUXI ON dba.asientosdet.Cod_Empresa = DBA.PLANAUXI.Cod_Empresa AND dba.asientosdet.Periodo = DBA.PLANAUXI.Periodo AND dba.asientosdet.CodPlanCta = DBA.PLANAUXI.CodPlanCta AND dba.asientosdet.CodPlanAux = DBA.PLANAUXI.CodPlanAux),DBA.PLANCTA,dba.asientoscab,dba.tipoasiento "+
+							//"DBA.PLANCTA,dba.asientoscab,dba.tipoasiento,dba.asientosdet,DBA.PLANAUXI "+
 					  "WHERE "+
-							"( dba.asientosdet.Cod_Empresa = DBA.PLANCTA.Cod_Empresa ) AND ( dba.asientosdet.Cod_Empresa = DBA.PLANAUXI.Cod_Empresa ) AND ( dba.asientosdet.Periodo = DBA.PLANAUXI.Periodo ) "+
-							"AND ( dba.asientosdet.CodPlanCta = DBA.PLANAUXI.CodPlanCta ) AND ( dba.asientosdet.CodPlanAux = DBA.PLANAUXI.CodPlanAux ) AND ( dba.asientosdet.Periodo = DBA.PLANCTA.Periodo ) "+
-							"AND ( dba.asientosdet.Periodo = DBA.asientoscab.Periodo ) AND ( dba.asientosdet.CodPlanCta = DBA.PLANCTA.CodPlanCta ) AND ( dba.asientoscab.Cod_Empresa = dba.asientosdet.Cod_Empresa ) "+
-							"AND ( dba.asientoscab.NroTransac = dba.asientosdet.NroTransac ) AND ( dba.asientoscab.TipoAsiento = dba.tipoasiento.TipoAsiento ) AND ( dba.AsientosCAB.cod_empresa = '"+empresa+"' ) "+
-							"AND ( dba.AsientosCAB.periodo = "+periodo+" ) AND ( dba.asientoscab.TipoAsiento = '"+tipoasiento+"') AND (DBA.asientoscab.fechacarga BETWEEN '"+fechad+"' and '"+fechah+"') "
+							"( dba.asientosdet.Periodo = DBA.PLANCTA.Periodo ) "+
+							"AND ( dba.asientosdet.CodPlanCta = DBA.PLANCTA.CodPlanCta ) "+
+							"AND ( dba.asientoscab.Cod_Empresa = dba.asientosdet.Cod_Empresa ) "+
+							"AND ( dba.asientoscab.NroTransac  = dba.asientosdet.NroTransac ) "+
+							"AND ( dba.asientoscab.TipoAsiento = dba.tipoasiento.TipoAsiento ) " +
+							"AND ( dba.AsientosCAB.cod_empresa = '"+empresa+"' ) "+
+							//"AND ( dba.AsientosCAB.periodo = "+periodo+" ) "+
+							"AND ( dba.asientoscab.TipoAsiento = '"+tipoasiento+"') "+
+							"AND (DBA.asientoscab.fecha BETWEEN '"+fechad+"' and '"+fechah+"') "
+							//"( dba.asientosdet.Cod_Empresa = DBA.PLANCTA.Cod_Empresa ) AND ( dba.asientosdet.Cod_Empresa = DBA.PLANAUXI.Cod_Empresa ) AND ( dba.asientosdet.Periodo = DBA.PLANAUXI.Periodo ) "+
+							//"AND ( dba.asientosdet.CodPlanCta = DBA.PLANAUXI.CodPlanCta ) AND ( dba.asientosdet.CodPlanAux = DBA.PLANAUXI.CodPlanAux ) AND ( dba.asientosdet.Periodo = DBA.PLANCTA.Periodo ) "+
+							//"AND ( dba.asientosdet.Periodo = DBA.asientoscab.Periodo ) AND ( dba.asientosdet.CodPlanCta = DBA.PLANCTA.CodPlanCta ) AND ( dba.asientoscab.Cod_Empresa = dba.asientosdet.Cod_Empresa ) "+
+							//"AND ( dba.asientoscab.NroTransac = dba.asientosdet.NroTransac ) AND ( dba.asientoscab.TipoAsiento = dba.tipoasiento.TipoAsiento ) AND ( dba.AsientosCAB.cod_empresa = '"+empresa+"' ) "+
+							//"AND ( dba.AsientosCAB.periodo = "+periodo+" ) AND ( dba.asientoscab.TipoAsiento = '"+tipoasiento+"') AND (DBA.asientoscab.fecha BETWEEN '"+fechad+"' and '"+fechah+"') "
 		if(autorizado == 'SI')
 				string+="AND (DBA.asientoscab.autorizado = 'S') "
 		if(autorizado == 'NO')
 				string+="AND (DBA.asientoscab.autorizado = 'N') "
 			string+= "ORDER BY "+
 							"DBA.AsientosCAB.Cod_Empresa ASC,DBA.AsientosCAB.Fecha ASC,DBA.AsientosDet.NroTransac ASC,DBA.AsientosDet.Linea ASC"
-			
+		
 		conn.exec(string, function(err, row){
 			res.json({ data : row });
 		});
