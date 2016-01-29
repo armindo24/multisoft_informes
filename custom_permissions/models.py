18faba69824b93d1a083cc3f32813b7c95de3781
Lucas Marcelo Suárez <suarezlucas28@gmail.com>
2016-01-29 17:33:27 -0300

    a

--- custom_permissions/models.py
+++ custom_permissions/models.py
@@ -9,6 +9,11 @@ from django.db.models.loading import  get_models, get_app
 permissions_list = (("entrar_finanzas", "Finanzas"),
                    ("entrar_ventas", "Ventas"),
                    ("entrar_admin", "Admin"),
+                   ("informe_balancegeneral", "Balance General"),
+                   ("informe_balancegeneralcomprobado", "Balance General Comprobado"),
+                   ("informe_diario", "Libro Diario"),
+                   ("informe_mayorcuenta", "Libro Mayor de Cuenta"),
+                   ("informe_ordenpago", "Ordenes de Pago"),
                    )
 
 class CustomPermissions(models.Model):
--- finanzas/views.py
+++ finanzas/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template.context import RequestContext
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
+
+@permission_required('custom_permissions.informe_ordenpago')
+def ordenpago(request):
+    return render_to_response('finanzas/ordenpago.html', {}, context_instance=RequestContext(request))
+
+@permission_required('custom_permissions.informe_balancegeneral')
+def balancegeneral(request):
+    return render_to_response('finanzas/balancegeneral.html', {}, context_instance=RequestContext(request))
+
+@permission_required('custom_permissions.informe_balancegeneralcomprobado')
+def balancegeneral_comprobado(request):
+    return render_to_response('finanzas/balancegeneral_comprobado.html', {}, context_instance=RequestContext(request))
+
+@permission_required('custom_permissions.informe_diario')
+def diario_comprobado(request):
+    return render_to_response('finanzas/diario_comprobado.html', {}, context_instance=RequestContext(request))
+
+@permission_required('custom_permissions.informe_mayorcuenta')
+def mayor_cuentas(request):
+    return render_to_response('finanzas/mayor_cuentas.html', {}, context_instance=RequestContext(request))
