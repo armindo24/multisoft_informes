229636aa2f2c1631cddb6c9041599fc00a6e5fdd
Jordan <jayala1993@outlook.com>
2016-02-01 11:54:58 -0300

    Reorganizar express app

--- server/multisoft/app.js
+++ server/multisoft/app.js
@@ -5,8 +5,31 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var sqlanywhere = require('sqlanywhere');
+var conn = sqlanywhere.createConnection();
+
+var cstr = {
+    Host: '192.168.51.139:2638', Server: 'integrado',
+    UserID: 'dba', Password: 'ownnetmasterkey', DatabaseName: 'integrado'
+};
+
+conn.connect(cstr, function (err) {
+    console.log("conectado");
+
+    conn.exec("select Cod_Empresa,Des_Empresa from dba.EMPRESA", function (err, row) {
+        if (err) {
+            console.log("error");
+            console.log(err);
+        } else {
+            console.log("bien");
+            console.log(row);
+        }
+    });
+});
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -18,18 +41,19 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api/v1', api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,23 +61,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
---
+++ server/multisoft/routes/api.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+router.get('/empresa/select', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//comprobantes select option
+router.get('/comprobante/select/:empresa', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//proveedores select option
+router.get('/proveedor/select/:empresa/:tipo', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//sucursales select option
+router.get('/sucursal/select/:empresa', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//list orden de pago
+router.get('/ordenpago/list/:empresa/:sucursal/:tipoop/:comprobante/:tipoproveedor/:proveedor/:desde/:hasta', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//cuentas select option
+router.get('/cuenta/select/:empresa/:periodo', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//list balance general
+router.get('/balancegeneral/list/:empresa/:periodo/:cuentad/:cuentah/:mesd/:mesh/:nivel/:saldo/:moneda/:aux', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//list balance comprobado
+router.get('/balancecomprobado/list/:empresa/:periodo/:periodoant/:mes/:mesant/:nivel/:moneda', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//tipo asiento select option
+router.get('/tipoasiento/select/', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//list diario comprobado
+router.get('/diariocomprobado/list/:empresa/:tipoasiento/:fechad/:fechah/:autorizado', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//detalle de movimientos de cuenta libro mayor
+router.get('/mayorcuenta/det/:empresa/:periodo/:fechad/:fechah/:tipoasiento/:cuenta', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+//cabecera de movimientos de cuenta libro mayor
+router.get('/mayorcuenta/cab/:empresa/:periodo/:fechad/:fechah/:tipoasiento/:cuentad/:cuentah/:incluir', function (req, res, next) {
+    res.json({data: req.params});
+});
+
+module.exports = router;
