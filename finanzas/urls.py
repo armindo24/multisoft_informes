cbd528c72c34b1effd6f960d587b1d85897f4a5a
Jordan <jayala1993@outlook.com>
2016-02-05 09:36:56 -0300

    Add venta_resumido url

--- finanzas/urls.py
+++ finanzas/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^balancegeneral_comprobado/$',  'finanzas.views.balancegeneral_comprobado', name="balancegeneral_comprobado"),
     url(r'^diario_comprobado/$',  'finanzas.views.diario_comprobado', name="diario_comprobado"),
     url(r'^mayor_cuentas/$',  'finanzas.views.mayor_cuentas', name="mayor_cuentas"),
+    url(r'^venta_resumido/$',  'finanzas.views.venta_resumido', name="venta_resumido"),
 )
