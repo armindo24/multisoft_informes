7e650652d8882e63250555075db642dd1a473708
Jordan <jayala1993@outlook.com>
2016-02-12 12:08:51 -0300

    Excluir rechazados

--- server/multisoft/models/extractoCuenta.js
+++ server/multisoft/models/extractoCuenta.js
@@ -14,9 +14,9 @@ ExtractoCuenta.depositos = function (params, query, cb) {
     var columns = "date(d.fecha) as fecha, d.cuentabanco, d.nrodeposito, dd.tot_efectivo, d.observ";
     var table = "dba.depcuenta d";
     var join_table = "dba.depcuentadet dd";
-    var join_cond = "(dd.nrodeposito = d.nrodeposito and dd.Codbanco = d.Codbanco)";
-    var cond = "d.cod_empresa = ? and d.cuentabanco = ? and fecha between ? and ?";
-    var sql_params = [params.empresa, query.cuenta, query.fechad, query.fechah];
+    var join_cond = "(dd.nrodeposito = d.nrodeposito and dd.Codbanco = d.Codbanco and dd.cod_empresa = d.cod_empresa)";
+    var cond = "d.cod_empresa = ? and d.codbanco = ? and d.cuentabanco = ? and fecha between ? and ? and d.estado = 'A'";
+    var sql_params = [params.empresa, query.banco, query.cuenta, query.fechad, query.fechah];
     if (query.sucursal) {
         cond += " and d.cod_sucursal = ?";
         sql_params.push(query.sucursal);
@@ -40,7 +40,7 @@ ExtractoCuenta.extracciones = function (params, query, cb) {
     console.log(query);
     var columns = "date(e.fecha) as fecha, e.codbanco, e.extraccionnro, e.beneficiario, e.importe, e.observ";
     var table = "dba.extcuenta e";
-    var cond = "e.cod_empresa = ? and e.cuentabanco = ? and e.codbanco = ? and fecha between ? and ?";
+    var cond = "e.cod_empresa = ? and e.cuentabanco = ? and e.codbanco = ? and fecha between ? and ? and e.anulado = 'N' and e.estado = 'A'";
     var order = "fecha";
     var sql = util.format("SELECT %s FROM %s WHERE %s ORDER BY %s", columns, table, cond, order);
     var tot_sql = util.format("SELECT sum(e.importe) as total FROM %s WHERE %s", table, cond);
