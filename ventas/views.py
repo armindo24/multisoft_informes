090a29a6835a4a2e168903a0b13d1b183795158a
Jordan <jayala1993@outlook.com>
2016-02-26 09:06:19 -0300

    User id in context

--- ventas/views.py
+++ ventas/views.py
@@ -5,4 +5,4 @@ from django.contrib.auth.decorators import permission_required
 # TODO: Add permission
 @permission_required('custom_permissions.informe_ventascomprobante')
 def venta_resumido(request):
-    return render_to_response('ventas/resumido.html', {}, context_instance=RequestContext(request))
+    return render_to_response('ventas/resumido.html', {'user': request.user.id}, context_instance=RequestContext(request))
