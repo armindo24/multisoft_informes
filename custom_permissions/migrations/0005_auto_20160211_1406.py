3bd22006d65497bcfc4b628656f731ac331e3879
luke <suarezlucas28@gmail.com>
2016-03-04 11:15:50 -0300

    a

---
+++ custom_permissions/migrations/0005_auto_20160211_1406.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('custom_permissions', '0004_auto_20160209_1919'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='custompermissions',
+            options={'permissions': (('entrar_finanzas', 'Finanzas'), ('entrar_ventas', 'Ventas'), ('entrar_admin', 'Admin'), ('informe_centrocostos', 'Centro de Costos'))},
+        ),
+    ]
