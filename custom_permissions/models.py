5465209eb5aa2b979b1685edc0d815d4d74b5e73
luke <suarezlucas28@gmail.com>
2016-03-04 10:14:03 -0300

    a

--- custom_permissions/models.py
+++ custom_permissions/models.py
@@ -6,15 +6,17 @@ from django.db import models
 permissions_list = (("entrar_finanzas", "Finanzas"),
                    ("entrar_ventas", "Ventas"),
                    ("entrar_admin", "Admin"),
-                   #("informe_balancegeneral", "Balance General"),
-                   #("informe_balancegeneralcomprobado", "Balance General Comprobado"),
-                   #("informe_diario", "Libro Diario"),
-                   #("informe_mayorcuenta", "Libro Mayor de Cuenta"),
-                   #("informe_ordenpago", "Ordenes de Pago"),
-                   #("informe_mayorcuentaauxiliar", "Libro Mayor de Auxiliares"),
-                   #("informe_ventascomprobante", "Ventas por Comprobante"),
-                   #("informe_extractocuenta", "Extracto de Cuenta Bancaria")
-                   # ('informe_centrocostos', "Centro de Costos"),
+                   ("informe_balancegeneral", "Balance General"),
+                   ("informe_balancegeneralcomprobado", "Balance General Comprobado"),
+                   ("informe_diario", "Libro Diario"),
+                   ("informe_mayorcuenta", "Libro Mayor de Cuenta"),
+                   ("informe_ordenpago", "Ordenes de Pago"),
+                   ("informe_mayorcuentaauxiliar", "Libro Mayor de Auxiliares"),
+                   ("informe_ventascomprobante", "Ventas por Comprobante"),
+                   ("informe_extractocuenta", "Extracto de Cuenta Bancaria"),
+                   ("informe_centrocostos", "Centro de Costos"),
+                   ("informe_bienactivo", "Bien Activo"),
+                   ("informe_flujofondo", "Flujo de Fondo"),
                    )
 
 class CustomPermissions(models.Model):
