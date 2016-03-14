baeb349287433998805e58c69a110867983cf3e8
luke <suarezlucas28@gmail.com>
2016-03-14 10:21:21 -0300

    aa

--- server/multisoft/models/compras.js
+++ server/multisoft/models/compras.js
@@ -1,5 +1,6 @@
 var conn = require('../db');
 var util = require('util');
+var q = require('./queryUtils');
 var Compras = {};
 
 Compras.all = function (filters, cb) {
@@ -8,7 +9,7 @@ Compras.all = function (filters, cb) {
     var select = ""
     
     if (filters.moneda == "GS"){
-        select = "case when dba.FACTCAB.anulado = 'N' then 'Activo' else 'Anulado' end as estado,DBA.EMPRESA.Des_Empresa,DBA.FACTCAB.CodProv,DBA.FACTCAB.NroFact,date(DBA.FACTCAB.FechaFact) as FechaFact,DBA.FACTCAB.Cod_Tp_Comp,"+ 
+        select = "case when dba.FACTCAB.anulado = 'N' then 'Activo' else 'Anulado' end as estado,DBA.EMPRESA.Des_Empresa,DBA.FACTCAB.CodProv,DBA.FACTCAB.NroFact,date(DBA.FACTCAB.FechaFact) as FechaFact,DBA.FACTCAB.Cod_Tp_Comp,DBA.TPOCBTE.Des_Tp_Comp,"+ 
                 "DBA.FACTCAB.Cod_Sucursal,DBA.FACTCAB.CodDpto,DBA.FACTCAB.CodMoneda,DBA.FACTCAB.Dcto,DBA.FACTCAB.TotalGrav,"+ 
                 "cast(DBA.FACTCAB.IVA as decimal (20,0)) as IVA,DBA.FACTCAB.FactCambio,cast(DBA.FACTCAB.TotalExen as decimal (20,0)) as TotalExen,DBA.FACTCAB.IVAIncluido,DBA.DPTO.coddpto,"+ 
                 "DBA.DPTO.descrip,DBA.MONEDA.Descrip,DBA.MONEDA.cantdecimal,DBA.Proveed.RazonSocial,DBA.SUCURSAL.des_sucursal,"+ 
@@ -18,7 +19,7 @@ Compras.all = function (filters, cb) {
                 "isnull(DBA.FactCab.Timbrado,DBA.Proveed.Timbrado,DBA.FactCab.Timbrado) as Timbrado,"+
                 "cast(dba.FACTCAB.IVA+DBA.FACTCAB.TotalExen+(case when FACTCAB.IVAIncluido = 'S' then  DBA.FACTCAB.TotalGrav - DBA.FACTCAB.IVA else DBA.FACTCAB.TotalGrav end) as decimal (20,0)) as total";
     } else {
-        select = "case when dba.FACTCAB.anulado = 'N' then 'Activo' else 'Anulado' end as estado,DBA.EMPRESA.Des_Empresa,DBA.FACTCAB.CodProv,DBA.FACTCAB.NroFact,date(DBA.FACTCAB.FechaFact) as FechaFact,DBA.FACTCAB.Cod_Tp_Comp,"+ 
+        select = "case when dba.FACTCAB.anulado = 'N' then 'Activo' else 'Anulado' end as estado,DBA.EMPRESA.Des_Empresa,DBA.FACTCAB.CodProv,DBA.FACTCAB.NroFact,date(DBA.FACTCAB.FechaFact) as FechaFact,DBA.FACTCAB.Cod_Tp_Comp,DBA.TPOCBTE.Des_Tp_Comp,"+ 
                 "DBA.FACTCAB.Cod_Sucursal,DBA.FACTCAB.CodDpto,DBA.FACTCAB.CodMoneda,DBA.FACTCAB.Dcto,DBA.FACTCAB.TotalGrav,"+ 
                 "cast(DBA.FACTCAB.IVA as decimal (20,2)) as IVA,DBA.FACTCAB.FactCambio,cast(DBA.FACTCAB.TotalExen as decimal (20,2)) as TotalExen,DBA.FACTCAB.IVAIncluido,DBA.DPTO.coddpto,"+ 
                 "DBA.DPTO.descrip,DBA.MONEDA.Descrip,DBA.MONEDA.cantdecimal,DBA.Proveed.RazonSocial,DBA.SUCURSAL.des_sucursal,"+ 
@@ -34,12 +35,9 @@ Compras.all = function (filters, cb) {
                 "and ( DBA.FACTCAB.Cod_Empresa = DBA.TPOCBTE.Cod_Empresa ) and ( DBA.FACTCAB.Cod_Tp_Comp = DBA.TPOCBTE.cod_tp_comp ) "+ 
                 "and ( DBA.FACTCAB.Cod_Empresa = DBA.PROVEED.Cod_Empresa ) and ( DBA.FACTCAB.CodProv = DBA.PROVEED.CodProv ) "+ 
                 "and ( DBA.FACTCAB.Cod_Empresa = DBA.DPTO.cod_empresa ) and ( DBA.FACTCAB.Cod_Sucursal = DBA.DPTO.cod_sucursal ) "+ 
-                "and ( DBA.FACTCAB.CodDpto = DBA.DPTO.coddpto ) AND ( FactCab.cod_empresa = '"+filters.empresa+"') AND (FACTCAB.Cod_Sucursal = '01') "+
-                "AND (FACTCAB.CodDpto = '01') AND (FACTCAB.CodMoneda = 'GS') AND ( DATE (DBA.FACTCAB.FechaFact) >= DATE ('2015-01-01') ) "+  
-                "AND ( DATE (DBA.FACTCAB.FechaFact) <= DATE ('2015-12-31') )";
-//                 "and ( DBA.FACTCAB.CodDpto = DBA.DPTO.coddpto ) AND ( FactCab.cod_empresa = '"+filters.empresa+"') AND (FACTCAB.Cod_Sucursal = '"+filters.sucursal+"') "+
-//                 "AND (FACTCAB.CodDpto = '"+filters.departamento+"') AND (FACTCAB.CodMoneda = '"+filters.moneda+"') AND ( DATE (DBA.FACTCAB.FechaFact) >= DATE ('"+filters.compras_start+"') ) "+  
-//                 "AND ( DATE (DBA.FACTCAB.FechaFact) <= DATE ('"+filters.compras_end+"') )";
+                "and ( DBA.FACTCAB.CodDpto = DBA.DPTO.coddpto ) AND ( FactCab.cod_empresa = '"+filters.empresa+"') AND (FACTCAB.Cod_Sucursal = '"+filters.sucursal+"') "+
+                "AND (FACTCAB.CodDpto = '"+filters.departamento+"') AND (FACTCAB.CodMoneda = '"+filters.moneda+"') AND ( DATE (DBA.FACTCAB.FechaFact) >= DATE ('"+filters.compras_start+"') ) "+  
+                "AND ( tpocbte.tp_def <> 'RT') AND ( DATE (DBA.FACTCAB.FechaFact) <= DATE ('"+filters.compras_end+"') )";
 
 //     if (filters.cliente) {
 //         where += " and (dba.vtacab.cod_cliente = ?)";
@@ -51,10 +49,10 @@ Compras.all = function (filters, cb) {
 //         args.push(filters.sucursal);
 //     }
 // 
-//     if (filters.tipo_comprobante) {
-//         where += " and (dba.vtacab.cod_tp_comp = ?)";
-//         args.push(filters.tipo_comprobante);
-//     }
+    console.log(filters.tipooc)
+    if (filters.tipooc) {
+        sql += " AND (dba.FACTCAB.Cod_Tp_Comp IN " + q.in(filters.tipooc) + ") ";
+    } 
 // 
 //     if (filters.desde && filters.hasta) {
 //         where += " and (dba.vtacab.fha_cbte BETWEEN ? and ?)";
@@ -64,15 +62,15 @@ Compras.all = function (filters, cb) {
 
 
     var sql = util.format("SELECT %s FROM %s WHERE %s", select, from, where);
-//     if (filters.order) {
-//         if (filters.order == "cod_tp_comp") {
-//             sql += " ORDER BY dba.vtacab.Cod_Tp_Comp";
-//         } else if (filters.order == "cod_cliente") {
-//             sql += " ORDER BY dba.vtacab.cod_cliente";
-//         } else if (filters.order == "cod_vendedor") {
-//             sql += " ORDER BY dba.vtacab.cod_vendedor";
-//         }
-//     }
+    if (filters.agrupar) {
+        if (filters.agrupar == "Cod_Tp_Comp") {
+            sql += " ORDER BY dba.FACTCAB.Cod_Tp_Comp";
+        } else if (filters.agrupar == "RazonSocial") {
+            sql += " ORDER BY dba.FACTCAB.CodProv";
+        } else if (filters.agrupar == "AR") {
+            //sql += " ORDER BY dba.FACTCAB.cod_vendedor";
+        }
+    }
 
     console.log(sql);
 
