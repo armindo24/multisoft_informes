2a579a4bfe598c39a9a8cce2f2a423770842d225
Jordan <jayala1993@outlook.com>
2016-03-14 10:04:51 -0300

    Presupuesto movido a ventas

--- compras/urls.py
+++ compras/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^ordencompra/$',  'compras.views.ordencompra', name="ordencompra"),
-    url(r'^compras/$',  'compras.views.compras', name="compras"),
-    url(r'^presupuesto/$', views.presupuesto, name='presupuesto')
+    url(r'^compras/$',  'compras.views.compras', name="compras")
 )
