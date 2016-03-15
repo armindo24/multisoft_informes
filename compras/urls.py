626371c0b78adb909972b0ece98ac29155be4d0c
luke <suarezlucas28@gmail.com>
2016-03-15 10:04:36 -0300

    a

--- compras/urls.py
+++ compras/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^ordencompra/$',  'compras.views.ordencompra', name="ordencompra"),
-    url(r'^compras/$',  'compras.views.compras', name="compras")
+    url(r'^compras/$',  'compras.views.compras', name="compras"),
+    url(r'^compras_articulo/$',  'compras.views.compras_articulo', name="compras_articulo")
 )
