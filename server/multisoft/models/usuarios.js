edfad1eee609468e009fe0e0ef640bcdf725dda3
Jordan <jayala1993@outlook.com>
2016-04-08 16:38:44 -0400

    Compras usuarios

--- server/multisoft/models/usuarios.js
+++ server/multisoft/models/usuarios.js
@@ -3,7 +3,9 @@ var conn = require('../db');
 var Usuarios = {};
 
 Usuarios.compras = function (params, cb) {
-    conn.exec("Select lower ( cod_usuario ) as cod_usuario From dba.usuarios where empresas = '" + params.empresa + "' order by cod_usuario", function (err, row) {
+    conn.exec("Select lower ( cod_usuario ) as " +
+        "cod_usuario From dba.usuarios where empresas = '" + params.empresa +
+        "' order by cod_usuario", function (err, row) {
         if (err) throw err;
         cb(row);
     });
