2eb1ec0e511b3d201aae95954d044081a91c8a85
Lucas Suárez <suarezlucas28@gmail.com>
2016-04-22 17:48:38 -0400

    terminado legajos

--- server/multisoft/models/legajos_sueldo.js
+++ server/multisoft/models/legajos_sueldo.js
@@ -6,14 +6,32 @@ var Legajos = {};
 Legajos.all = function (filters, cb) {
     //conn.exec("SET ROWCOUNT 100"); //TODO: solucionar resultados muy grandes
 
-    var select = "dba.legajo.cod_empresa, dba.legajo.codempleado, dba.legajo.fecha, dba.legajo.codmvto, dba.legajo.aprobado, dba.legajo.sucanterior, "+           
-                 "dba.legajo.dptoanterior, dba.legajo.cargoanterior, dba.legajo.sueldoanterior, dba.legajo.sucactual, dba.legajo.dptoactual, "+           
-                 "dba.legajo.cargoactual, dba.legajo.sueldoactual, dba.legajo.porcvariacion, dba.legajo.observacion, dba.empleados.apellidos, "+           
-                 "dba.empleados.nombres, dba.empleados.cedula, date(dba.empleados.fechanac) as fechanac, dba.empleados.sexo, case when dba.empleados.estcivil = 'S' then 'Soltero' else (case when dba.empleados.estcivil = 'C' then 'Casado' else (case when dba.empleados.estcivil = 'D' then 'Divorciado' else (case when dba.empleados.estcivil = 'O' then 'Otros' end) end) end) end as estcivil, "+ 
+    var select = "id = identity(8), dba.legajo.cod_empresa, dba.legajo.codempleado, "+
+                 "date(dba.legajo.fecha) as fecha, "+
+                 "dba.legajo.codmvto, "+
+                 "case when dba.legajo.aprobado = 'S' then 'Si' else 'No' end as aprobado,"+
+                 "isnull(dba.legajo.sucanterior,'',dba.legajo.sucanterior) as sucanterior, "+
+                 "isnull(dba.legajo.dptoanterior,'',dba.legajo.dptoanterior) as dptoanterior, "+         
+                 "dba.legajo.cargoanterior, "+
+                 "cast(dba.legajo.sueldoanterior as decimal (20,0)) as sueldoanterior, "+
+                 "dba.legajo.sucactual, dba.legajo.dptoactual, "+           
+                 "dba.legajo.cargoactual, "+
+                 "cast(dba.legajo.sueldoactual as decimal (20,0)) as sueldoactual, "+
+                 "dba.legajo.porcvariacion, dba.legajo.observacion, dba.empleados.apellidos, "+           
+                 "dba.empleados.nombres, dba.empleados.cedula, "+
+                 "case when cast(dba.empleados.fechanac as varchar) = '' or cast(dba.empleados.fechanac as varchar) is null then '' else cast(date(dba.empleados.fechanac) as varchar) end as fechanac, "+
+                 "dba.empleados.sexo, case when dba.empleados.estcivil = 'S' then 'Soltero' else (case when dba.empleados.estcivil = 'C' then 'Casado' else (case when dba.empleados.estcivil = 'D' then 'Divorciado' else (case when dba.empleados.estcivil = 'O' then 'Otros' end) end) end) end as estcivil, "+ 
                  "case when dba.empleados.aportaips = 'S' then 'Si' else 'No' end as aportaips,"+           
-                 "dba.empleados.nroips, dba.empleados.direccion, isnull(dba.empleados.telefono,'',dba.empleados.telefono) as telefono, date(dba.empleados.fechaentrada) as fechaentrada, dba.empresa.des_empresa, "+
-                 "dba.mvtolegajo.descrip, sucursal_a.des_sucursal, dpto_a.descrip, cargos_a.descrip, sucursal_b.des_sucursal, dpto_b.descrip, cargos_b.descrip ";
-    
+                 "isnull(dba.empleados.nroips,'',dba.empleados.nroips) as nroips, "+
+                 "isnull(dba.empleados.direccion,'',dba.empleados.direccion) as direccion, isnull(dba.empleados.telefono,'',dba.empleados.telefono) as telefono, date(dba.empleados.fechaentrada) as fechaentrada, dba.empresa.des_empresa, "+
+                 "dba.mvtolegajo.descrip as dmovimiento, "+
+                 "isnull(sucursal_a.des_sucursal,'',sucursal_a.des_sucursal) as suc_anterior, "+
+                 "isnull(sucursal_b.des_sucursal,'',sucursal_b.des_sucursal) as suc_actual, "+
+                 "isnull(dpto_a.descrip,'',dpto_a.descrip) as dpto_anterior, "+
+                 "isnull(dpto_b.descrip,'',dpto_b.descrip) as dpto_actual, "+
+                 "isnull(cargos_a.descrip,'',cargos_a.descrip) as car_anterior, "+
+                 "isnull(cargos_b.descrip,'',cargos_b.descrip) as car_actual";
+                 
     var from = "(dba.legajo LEFT OUTER JOIN dba.sucursal sucursal_a ON dba.legajo.cod_empresa = sucursal_a.cod_empresa AND dba.legajo.sucanterior = sucursal_a.cod_sucursal "+
                "LEFT OUTER JOIN dba.dpto dpto_a ON dba.legajo.cod_empresa = dpto_a.cod_empresa AND dba.legajo.sucanterior = dpto_a.cod_sucursal AND dba.legajo.dptoanterior = dpto_a.coddpto "+
                "LEFT OUTER JOIN dba.cargos cargos_a ON dba.legajo.cod_empresa = cargos_a.cod_empresa AND dba.legajo.cargoanterior = cargos_a.codcargo LEFT OUTER JOIN dba.sucursal sucursal_b ON "+
@@ -35,7 +53,7 @@ Legajos.all = function (filters, cb) {
         sql += " AND (dba.legajo.codmvto IN " + q.in(filters.tipo) + ") ";
     } 
     
-    sql += " ORDER BY dba.legajo.codempleado";
+    sql += " ORDER BY dba.legajo.fecha ASC";
 
     console.log(sql);
 
