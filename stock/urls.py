9ada39ee9217c1a1e83520fd7592fbbd3cbb90aa
Jordan <jayala1993@outlook.com>
2016-05-03 20:14:50 -0400

    init stock valorizado

--- stock/urls.py
+++ stock/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^articulos/$', views.articulos, name='articulos'),
     url(r'^precios/$', views.precios, name='precios'),
     url(r'^existencia_deposito/$', views.existencia_deposito, name='existencia_deposito'),
+    url(r'^valorizado/$', views.valorizado, name='valorizado')
 ]
