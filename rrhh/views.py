c013534994b75bee275aebb990406ba134716348
Lucas Suárez <suarezlucas28@gmail.com>
2016-05-10 13:33:35 -0400

    permisos agregados

--- rrhh/views.py
+++ rrhh/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render, render_to_response
 from django.template.context import RequestContext
 
 # Create your views here.
-#@permission_required('custom_permissions.informe_ventascomprobante')
+@permission_required('custom_permissions.informe_legajos')
 def legajos(request):
     return render_to_response('rrhh/legajos.html', {'user': request.user.id}, context_instance=RequestContext(request))
 
+@permission_required('custom_permissions.informe_sueldosjornales')
 def sueldos_jornales(request):
     return render_to_response('rrhh/sueldos_jornales.html', {'user': request.user.id}, context_instance=RequestContext(request))
 
+@permission_required('custom_permissions.informe_anticipos')
 def anticipos(request):
     return render_to_response('rrhh/anticipos.html', {'user': request.user.id}, context_instance=RequestContext(request))
 
+@permission_required('custom_permissions.informe_aguinaldos')
 def aguinaldos(request):
     return render_to_response('rrhh/aguinaldos.html', {'user': request.user.id}, context_instance=RequestContext(request))
 
+@permission_required('custom_permissions.informe_recibos')
 def recibos(request):
     return render_to_response('rrhh/recibos.html', {'user': request.user.id}, context_instance=RequestContext(request))
