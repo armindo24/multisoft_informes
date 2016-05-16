4aed6d4177cf4200051e87fa7261ca2eac9f2c4a
Jordan <jayala1993@outlook.com>
2016-05-16 11:35:21 -0400

    Development settings without auth

--- server/multisoft/app.js
+++ server/multisoft/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('config');
-var redis = require('redis'), client = redis.createClient('/tmp/redis.sock');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -16,50 +15,17 @@ var stock = require('./routes/stock');
 
 var app = express();
 
-if (process.env.hasOwnProperty('DYLD_LIBRARY_PATH')) {
-    console.log(process.env.DYLD_LIBRARY_PATH);
-} else {
-    console.log('Warning: DYLD_LIBRARY_PATH no esta definido.');
-}
-
-if (process.env.hasOwnProperty('LD_LIBRARY_PATH')) {
-    console.log(process.env.LD_LIBRARY_PATH);
-} else {
-    console.log('Warning: LD_LIBRARY_PATH no esta definido.');
-}
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(cors()); //TODO: revisar seguridad al usar cors
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function (req, res, next) {
-    if (req.cookies && req.cookies.sessionid) {
-        var sessionid = 'session:' + req.cookies['sessionid'];
-        client.get(sessionid, function (err, data) {
-            if (data) {
-                var sessionData = new Buffer(data, 'base64').toString();
-                var sessionObjString = sessionData.substring(sessionData.indexOf(":") + 1);
-                var sessionObjJSON = JSON.parse(sessionObjString);
-                req.user = sessionObjJSON['_auth_user_id'];
-                req.loggedin = true;
-                next();
-            } else {
-                res.status(401).send('Unauthorized');
-            }
-        });
-    } else {
-        res.status(401).send('Unauthorized');
-    }
-});
 
 // Register routes
 app.use('/', routes);
@@ -87,7 +53,34 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
+    app.use(cors());
+    app.use(function (req, res, next) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        next();
+    });
+} else {
+    var redis = require('redis'), client = redis.createClient('/tmp/redis.sock');
+    app.use(function (req, res, next) {
+        if (req.cookies && req.cookies.sessionid) {
+            var sessionid = 'session:' + req.cookies['sessionid'];
+            client.get(sessionid, function (err, data) {
+                if (data) {
+                    var sessionData = new Buffer(data, 'base64').toString();
+                    var sessionObjString = sessionData.substring(sessionData.indexOf(":") + 1);
+                    var sessionObjJSON = JSON.parse(sessionObjString);
+                    req.user = sessionObjJSON['_auth_user_id'];
+                    req.loggedin = true;
+                    next();
+                } else {
+                    res.status(401).send('Unauthorized');
+                }
+            });
+        } else {
+            res.status(401).send('Unauthorized');
+        }
+    });
 }
+
 
 // production error handler
 // no stacktraces leaked to user
