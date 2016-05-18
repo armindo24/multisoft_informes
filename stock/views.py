0d3ec89575f68a30b2d16c0c627c6c16e3dcc6f2
Lucas Suárez <suarezlucas28@gmail.com>
2016-05-18 17:57:46 -0400

    add premissions

--- stock/views.py
+++ stock/views.py
@@ -3,27 +3,27 @@ from django.template.context import RequestContext
 from django.contrib.auth.decorators import permission_required
 
 
-# TODO: Permiso
+@permission_required('custom_permissions.informe_articulos')
 def articulos(request):
     return render_to_response('stock/articulos.html', {'user': request.user.id},
                               context_instance=RequestContext(request))
 
-# TODO: Permiso
+@permission_required('custom_permissions.informe_precios')
 def precios(request):
     return render_to_response('stock/precios.html', {'user': request.user.id},
                               context_instance=RequestContext(request))
 
-# TODO: Permiso
+@permission_required('custom_permissions.informe_depositos')
 def existencia_deposito(request):
     return render_to_response('stock/existencia_deposito.html', {'user': request.user.id},
                               context_instance=RequestContext(request))
 
-# TODO: Permiso
+@permission_required('custom_permissions.informe_valorizado')
 def valorizado(request):
     return render_to_response('stock/valorizado.html', {'user': request.user.id},
                               context_instance=RequestContext(request))
 
-# TODO: Permiso
+@permission_required('custom_permissions.informe_ventascomprobante')
 def ficha_productos(request):
     return render_to_response('stock/ficha_productos.html', {'user': request.user.id},
                               context_instance=RequestContext(request))
