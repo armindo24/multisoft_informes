d0cbda2f0f21632a65482acc1e310bdd1f0bf26c
Lucas Suárez <suarezlucas28@gmail.com>
2016-05-25 14:46:07 -0400

    change on users

--- server/multisoft/routes/users.js
+++ server/multisoft/routes/users.js
@@ -4,6 +4,11 @@ var pg = require('../postgres');
 var _ = require('lodash');
 var Empresa = require('../models/empresa');
 
+router.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 // Create a new object, that prototypally inherits from the Error constructor
 function UserNotFoundError(message) {
     this.name = 'UserNotFoundError';
