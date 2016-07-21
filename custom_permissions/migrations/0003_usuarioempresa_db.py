9cbb870ddb2ed12e1bf81d934e6803b4cb6d3c62
Lucas Suárez <suarezlucas28@gmail.com>
2016-07-21 14:39:29 -0400

    fixed

--- custom_permissions/migrations/0003_usuarioempresa_db.py
+++ custom_permissions/migrations/0003_usuarioempresa_db.py
@@ -7,14 +7,10 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('custom_permissions', '0013_auto_20160511_1921'),
+        ('custom_permissions', '0002_auto_20160519_1628'),
     ]
 
     operations = [
-        migrations.AlterModelOptions(
-            name='custompermissions',
-            options={'permissions': (('entrar_finanzas', 'Finanzas'), ('entrar_ventas', 'Ventas'), ('entrar_stock', 'Stock'), ('entrar_compras', 'Compras'), ('entrar_rrhh', 'RRHH'), ('entrar_admin', 'Admin'))},
-        ),
         migrations.AddField(
             model_name='usuarioempresa',
             name='db',
