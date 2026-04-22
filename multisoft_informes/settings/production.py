776abdb90e3d8d052d194a36809c04ecf77bc34b
armindo24 <amedina@multisoft.com.py>
2026-04-22 16:42:41 -0300

    Fallback a sesiones DB en produccion

--- multisoft_informes/settings/production.py
+++ multisoft_informes/settings/production.py
@@ -1,4 +1,5 @@
 from .common import *
+import importlib.util
 
 DEBUG = False
 
@@ -12,14 +13,17 @@ DATABASES = {
     'default':
         {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'multisoft_informes',
+            'NAME': env('DB_NAME', default='multisoft_informes'),
             'USER': env('DB_USER'),
             'PASSWORD': env('DB_PASSWORD'),
-            'HOST': 'localhost',
-            'PORT': ''
+            'HOST': env('DB_HOST', default='localhost'),
+            'PORT': env('DB_PORT', default='')
         }
 }
 
-SESSION_ENGINE = 'redis_sessions.session'
-SESSION_REDIS_UNIX_DOMAIN_SOCKET_PATH = env('REDIS_SOCK')
-SESSION_REDIS_PREFIX = 'session'
+if importlib.util.find_spec('redis_sessions') is not None and env('REDIS_SOCK', default=''):
+    SESSION_ENGINE = 'redis_sessions.session'
+    SESSION_REDIS_UNIX_DOMAIN_SOCKET_PATH = env('REDIS_SOCK')
+    SESSION_REDIS_PREFIX = 'session'
+else:
+    SESSION_ENGINE = 'django.contrib.sessions.backends.db'
